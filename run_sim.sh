a7ce7524fae1d89796a8e9638d9b863a95584421
Aleksandar <aleksandarlilic03@gmail.com>
2024-01-07 17:30:28 +0100

    test(scripts): sync scripts to have the same default value for test_name and seed

--- run_sim.sh
+++ run_sim.sh
@@ -1,27 +1,34 @@
-#!/bin/bash -x
+#!/bin/bash
 set -Eeuo pipefail
 trap "echo -e \"\n\nERROR: Command failed at line: \${LINENO}.\n\n\"" ERR
 
-default_test_name="aes256_test"
-default_seed=10
+DEFAULT_TEST_NAME="aes256_test"
+DEFAULT_SEED=10
 test_name=""
 seed=""
-while getopts "t:s:" option; do
+get_defaults=false
+while getopts "t:s:g" option; do
 case "${option}" in
     t) test_name=${OPTARG};;
     s) seed=${OPTARG};;
+    g) get_defaults=true;;
     *) echo "Usage: run_sim.sh [-t <test_name>] [-s <seed>]"; exit 1;;
 esac
 done
 
+if [[ $get_defaults == true ]]; then
+    echo "$DEFAULT_TEST_NAME,$DEFAULT_SEED"
+    exit 0
+fi
+
 if [[ -z "$test_name" ]]; then
-    echo "Test name not specified, using default test: <$default_test_name>"
-    test_name="$default_test_name"
+    echo "Test name not specified, using default test: <$DEFAULT_TEST_NAME>"
+    test_name="$DEFAULT_TEST_NAME"
 fi
 
 if [[ -z $seed ]]; then
-    echo "Seed not specified, using default seed: <$default_seed>"
-    seed=$default_seed
+    echo "Seed not specified, using default seed: <$DEFAULT_SEED>"
+    seed=$DEFAULT_SEED
 fi
 
 sim_args="-testplusarg UVM_VERBOSITY=UVM_LOW -testplusarg UVM_TESTNAME=$test_name -sv_seed $seed -testplusarg EXIT_ON_ERROR"
--- setup_project.tcl
+++ setup_project.tcl
@@ -6,6 +6,12 @@ puts "Script assumes that the project is already open and the current working di
 
 puts "Setting up Project settings"
 
+# get defaults (seed and test) from shell script
+set defaults [exec run_sim.sh -g]
+set defaults_list [split $defaults ","]
+set test [lindex $defaults_list 0]
+set seed [lindex $defaults_list 1]
+
 # compilation
 set_property -name {xsim.compile.tcl.pre} -value {pre_compile.tcl} -objects [get_filesets sim_1]
 set_property -name {xsim.compile.xvlog.more_options} -value {-L uvm} -objects [get_filesets sim_1]
@@ -14,7 +20,8 @@ set_property -name {xsim.compile.xvlog.more_options} -value {-L uvm} -objects [g
 set_property -name {xsim.elaborate.xelab.more_options} -value {-L uvm -sv_lib aes_dpi.so} -objects [get_filesets sim_1]
 
 # simulation
-set_property -name {xsim.simulate.xsim.more_options} -value {-testplusarg UVM_VERBOSITY=UVM_HIGH -sv_seed 2} -objects [get_filesets sim_1]
+set xsim_opts "-testplusarg UVM_VERBOSITY=UVM_HIGH -testplusarg UVM_TESTNAME=$test -sv_seed $seed"
+set_property -name {xsim.simulate.xsim.more_options} -value $xsim_opts -objects [get_filesets sim_1]
 set_property -name {xsim.simulate.runtime} -value {100us} -objects [get_filesets sim_1]
 
 # Use all available threads for compilation and elaboration
